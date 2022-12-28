63bd8eca1b69830c5b68088c4a4c23a1ab9f8a3a
Taufiq Pohan <m.taufiq30s@hotmail.com>
2022-12-28 22:03:50 +0700

    Split VN execution command to a controller
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

--- commands/visualnovel.js
+++ commands/visualnovel.js
@@ -1,43 +1,9 @@
 import { SlashCommandBuilder } from 'discord.js';
-import vn_search from '../services/vn_search.js';
-import vn_info_tools from '../events/vnInfoToolCollect.js';
 import logger from '../services/logger_service.js';
 import embed from '../helpers/embed.js';
+import { info, search, team } from '../controllers/VisualNovelController.js';
 
-// Handle VN Search navigation collect
-async function handle(interaction, originInteraction, navigationCollect, client) {
-	try {
-		if (interaction.user.id != originInteraction.user.id) {
-			throw new Error('The user id does not match the requesting user id');
-		}
-		if (interaction.isButton()) {
-			await interaction.deferUpdate();
-			if (interaction.customId.includes('next-page')) {
-				this.resetTimer();
-				const page = parseInt(interaction.customId.split('-')[3]);
-				const title = interaction.customId.split('-')[0];
-				const data = await vn_search.search(title, client, page);
-				await interaction.editReply(data);
-			}
-			else if (interaction.customId.includes('prev-page')) {
-				navigationCollect.resetTimer();
-				const page = parseInt(interaction.customId.split('-')[3]);
-				const title = interaction.customId.split('-')[0];
-				const data = await vn_search.search(title, client, page);
-				await interaction.editReply(data);
-			}
-		}
-		else if (interaction.isStringSelectMenu() && interaction.customId === 'selected-result') {
-			const info = await vn_search.info(parseInt(interaction.values[0]), client);
-			const message = await originInteraction.followUp(info);
-			await vn_info_tools(originInteraction, client, message);
-			navigationCollect.stop(['VN was Selected']);
-		}
-	}
-	catch (err) {
-		throw err;
-	}
-}
+
 
 export default {
 	data: new SlashCommandBuilder()
@@ -63,43 +29,30 @@ export default {
 						.setRequired(true),
 				),
 		)
+		.addSubcommand(subcommand => 
+			subcommand
+				.setName('fantl')
+				.setDescription('Get all Visual Novel FanTL or find FanTL by team name')
+				.addStringOption(option =>
+					option.setName('team_name')
+						.setDescription('FanTL team name or keep it empty to get all teams')
+				)
+		)
 		.setDMPermission(false),
 
 
 	async execute(interaction, client) {
 		try {
-			// VN Info
 			if (interaction.options.getSubcommand() === 'info') {
-				await interaction.deferReply();
-				const result = await vn_search.info(interaction.options.getInteger('id'), client);
-				await interaction.editReply(result);
-
-				vn_info_tools(interaction, client);
+				await info(interaction, client);
 			}
-			// VN Search
+			
 			else if (interaction.options.getSubcommand() === 'search') {
-				// Send Search Result
-				await interaction.deferReply();
-				const result = await vn_search.search(interaction.options.getString('title'), client);
-				await interaction.editReply(result);
-
-				const navigationCollect = interaction.channel.createMessageComponentCollector({
-					time: 15000,
-				});
-
-				navigationCollect.on('collect', async (i) => {
-					try {
-						await handle(i, interaction, navigationCollect, client);
-					} catch(error) {
-						logger.error(error);
-						await interaction.followUp({ embeds: [embed.errorEmbed('Error', 'Waaahhhh....!!! An error was occured.\nPlease try again...~', client)] });
-						navigationCollect.stop(['VN was Selected']);
-					}
-				});
-
-				navigationCollect.on('end', async () => {
-					await interaction.deleteReply();
-				});
+				await search(interaction, client);
+			}
+			// FanTL Search
+			else if(interaction.options.getSubcommand() === 'fantl') {
+				await team(interaction, client);
 			}
 		}
 		catch (err) {
---
+++ controllers/VisualNovelController.js
@@ -0,0 +1,84 @@
+import vn_search from "../services/vn_search.js";
+import vn_info_tools from '../events/vnInfoToolCollect.js';
+
+// Handle VN Search navigation collect
+async function handle(interaction, originInteraction, navigationCollect, client) {
+    try {
+        if (interaction.user.id != originInteraction.user.id) {
+            throw new Error('The user id does not match the requesting user id');
+        }
+        if (interaction.isButton()) {
+            await interaction.deferUpdate();
+            if (interaction.customId.includes('next-page')) {
+                this.resetTimer();
+                const page = parseInt(interaction.customId.split('-')[3]);
+                const title = interaction.customId.split('-')[0];
+                const data = await vn_search.search(title, client, page);
+                await interaction.editReply(data);
+            }
+            else if (interaction.customId.includes('prev-page')) {
+                navigationCollect.resetTimer();
+                const page = parseInt(interaction.customId.split('-')[3]);
+                const title = interaction.customId.split('-')[0];
+                const data = await vn_search.search(title, client, page);
+                await interaction.editReply(data);
+            }
+        }
+        else if (interaction.isStringSelectMenu() && interaction.customId === 'selected-result') {
+            const info = await vn_search.info(parseInt(interaction.values[0]), client);
+            const message = await originInteraction.followUp(info);
+            await vn_info_tools(originInteraction, client, message);
+            navigationCollect.stop(['VN was Selected']);
+        }
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+// VN Info
+export const info = async (interaction, client) => {
+    await interaction.deferReply();
+    const result = await vn_search.info(interaction.options.getInteger('id'), client);
+    await interaction.editReply(result);
+
+    vn_info_tools(interaction, client);
+}
+
+// VN Search
+export const search = async (interaction, client) => {
+    // Send Search Result
+    await interaction.deferReply();
+    const result = await vn_search.search(interaction.options.getString('title'), client);
+    await interaction.editReply(result);
+
+    const navigationCollect = interaction.channel.createMessageComponentCollector({
+        time: 15000,
+    });
+
+    navigationCollect.on('collect', async (i) => {
+        try {
+            await handle(i, interaction, navigationCollect, client);
+        } catch (error) {
+            logger.error(error);
+            await interaction.followUp({ embeds: [embed.errorEmbed('Error', 'Waaahhhh....!!! An error was occured.\nPlease try again...~', client)] });
+            navigationCollect.stop(['vn_info_tools was Selected']);
+        }
+    });
+
+    navigationCollect.on('end', async () => {
+        await interaction.deleteReply();
+    });
+}
+
+// FanTL Team Search
+export const team = async (interaction, client) => {
+    try {
+        console.log('TODO'); // TODO : Fantl search
+        await interaction.reply({ embeds: [embed.embed(client.user.avatarURL, 'Coming Soon', 'Tch.. Tch.. Tch...\nFitur ini masih pengembangan loh, nanti ATRI kabarkan kalau udah selesai yah...~', 0xFFB100, null, null, null, 'https://cdn.discordapp.com/attachments/889918535139201064/929424964757635102/unknown.png')] });
+    } catch (error) {
+        logger.error(error);
+        await interaction.followUp({ embeds: [embed.errorEmbed('Error', 'Waaahhhh....!!! An error was occured.\nPlease try again...~', client)] });
+        navigationCollect.stop(['vn_info_tools was Selected']);
+    }
+}
