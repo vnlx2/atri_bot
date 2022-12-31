291b9ce8a41841b40e343874654dace00eb36769
Taufiq Pohan <m.taufiq30s@hotmail.com>
2022-12-31 20:46:08 +0700

    services/vndb_service: Remove unused code
    Remove findByTitleOld and clearConnection because we not
    using vndb-api when find vn.
    
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

--- services/vndb_service.js
+++ services/vndb_service.js
@@ -90,55 +90,7 @@ const findByTitle = async (client, name, page = 1) => {
 	}
 };
 
-const findByTitleOld = async (client, name, page = 1) => {
-	try {
-		return vndb
-			.query('get vn basic (search ~ "' + name + '") { "page": ' + page + ', "results": ' + 5 + ' }')
-			.then((res) => {
-				let results = '';
-				const items = [];
-				if (res.items.length === 0) {
-					return null;
-				}
-				else {
-					for (let i = 0; i < res.items.length; i++) {
-						const item = res.items[i];
-						if (i == 0) {
-							results += `${i + 1 + (5 * (page - 1))}. \`${item.id}\` - ${item.title}`;
-						}
-						else {
-							results += `\n${i + 1 + (5 * (page - 1))}. \`${item.id}\` - ${item.title}`;
-						}
-						items.push(
-							{
-								label: (item.title.length > 100) ? item.title.substring(0, 97) + '...' : item.title,
-								value: `${item.id}`,
-							},
-						);
-					}
-				}
-				return {
-					next_page: res.more,
-					results: embed_maker.embed(client.user.avatarURL, 'Search Results', results, 0x205bba),
-					items: items,
-				};
-			})
-			.catch((ex) => {
-				return ex;
-			});
-	}
-	catch (err) {
-		console.error(err);
-		logger.error(err);
-	}
-};
-
-const clearConnection = async () => {
-	vndb.destroy();
-};
-
 export default {
 	getInfo,
-	findByTitle,
-	clearConnection
+	findByTitle
 };
