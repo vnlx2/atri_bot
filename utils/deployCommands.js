356da2be8037c890b13ab533d3aa40da5b13dea8
Taufiq Pohan <m.taufiq30s@hotmail.com>
2022-12-31 20:54:22 +0700

    utis: Fail to deploy or remove command for VNL Server
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

--- utils/deployCommands.js
+++ utils/deployCommands.js
@@ -1,10 +1,10 @@
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord.js';
-import { config } from 'dotenv';
+import dotenv from 'dotenv';
 import { default as commands } from '../routes/commands.js';
 
 // Intialized env
-config();
+dotenv.config();
 
 const jsonCommands = [];
 for (const command of commands) {
@@ -18,7 +18,7 @@ rest.put(Routes.applicationGuildCommands(process.env.APP_ID, process.env.AKASHIC
 	.then(() => console.log('Application Commands register succesfully in Akashic Server.'))
 	.catch(console.error);
 
-if(!process.env.DEBUG_MODE) {
+if(process.env.DEBUG_MODE === 'false') {
 	rest.put(Routes.applicationGuildCommands(process.env.APP_ID, process.env.VNL_SERVER_ID), { body:jsonCommands })
 		.then(() => console.log('Application Commands register succesfully in VNL Server.'))
 		.catch(console.error);
--- utils/removeCommands.js
+++ utils/removeCommands.js
@@ -11,7 +11,7 @@ rest.put(Routes.applicationGuildCommands(process.env.APP_ID, process.env.AKASHIC
 	.then(() => console.log('Successfully deleted all guild commands in Akashic Server.'))
 	.catch(console.error);
 
-if(!process.env.DEBUG_MODE) {
+if(process.env.DEBUG_MODE === 'false') {
 	rest.put(Routes.applicationGuildCommands(process.env.APP_ID, process.env.VNL_SERVER_ID), { body:[] })
 		.then(() => console.log('Successfully deleted all guild commands in VNL Server.'))
 		.catch(console.error);
