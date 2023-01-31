8faf85ade39cb2090c0002cf0359145a45f5d082
Taufiq Pohan <m.taufiq30s@hotmail.com>
2023-01-31 23:54:32 +0700

    VisualNovelSearch: Add aliases column and regex
    To make the search results more comprehensive and it can search for titles
    from popular names or aliases. Add a search parameter based on aliases
    so that there are two parameter to be compared with 'or' logic
    and use the regex unsensitive case for search keywords.
    
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

--- services/VisualNovel/Jobs/FindJob.js
+++ services/VisualNovel/Jobs/FindJob.js
@@ -2,7 +2,8 @@ import VisualNovel from "../../../models/VisualNovel.js";
 
 export default async function (title, page = 1) {
     try {
-        return await VisualNovel.find({ title: new RegExp(title, 'i') }).collation({ locale: "en_US", numericOrdering: true })
+        const regex = new RegExp(title, "i");
+        return await VisualNovel.find({ $or:[{ title: regex }, { aliases: regex }] }).collation({ locale: "en_US", numericOrdering: true })
         .sort({ code: 1, title: 1 }).limit(5).skip(5 * (page - 1));
     } catch (err) {
         throw err;
