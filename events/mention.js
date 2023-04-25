d6000740fdb1fb5da162af232aa80fbdd25b9cfc
Taufiq Pohan <m.taufiq30s@hotmail.com>
2023-04-25 23:56:02 +0800

    Mention: Restructure mention response
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

--- events/mention.js
+++ events/mention.js
@@ -1,99 +1,148 @@
-import moment from 'moment';
+import {readFileSync} from 'fs';
 import dotenv from 'dotenv';
 import logger from '../services/logger_service.js';
 
+const conversations = JSON.parse(readFileSync('utils/arof/chat.json'));
 dotenv.config();
 
-// const vndbService = VndbService
+const preventMentions = ['@here', '@everyone', 'REPLY'];
 
 export default {
-	name: 'messageCreate',
-	once: false,
-	execute(message, client) {
-		try {
-			const random = ['Hi', 'Nani', 'Haiii....', 'Jangan panggil aku!', 'Kangen ya? hehe', 'Menjauh dasar mesum!', 'Sini peluk', 'Ada apa sayang?', ' Hentai!'];
+  name: 'messageCreate',
+  once: false,
+  async execute(message, client) {
+    try {
+      if (
+        message.guild.id === process.env.AKASHIC_SERVER_ID &&
+        message.content.includes(`<@${client.user.id}>`)
+      ) {
+        if (message.author.bot) return false;
+        if (preventMentions.includes(message.content)) return false;
 
-			// Cegak Bot Respon ketika mention all atau respon diri sendiri
-			if (message.author.bot) return false;
-			if (message.content.includes('@here') || message.content.includes('@everyone') || message.type == 'REPLY') return false;
+        logger.info(`${message.author.username} mentions Me`);
 
-			if (message.mentions.has(client.user.id)) {
-				console.info(`${message.author.username} mentions Me`);
-				logger.info(`${message.author.username} mentions Me`);
-				// Kalau ATRI Ultah (28 Agustus)
-				if (moment().month() == 7 && moment().date() == 28) {
-					console.log('Birthday Time');
-					logger.info('Birthday Time');
-					const content = message.content.toLowerCase();
-					if (content.includes('birthday') || content.includes('ulang tahun') || content.includes('tanjoubi')) {
-						message.channel.send(`Thank you ucapannya, <@${message.author.id}>... Atri bahagia deh jadinya :slight_smile:`);
-					}
-				}
-				// Kalau Mention Bot tanpa pesan
-				if (message.content === `<@${client.user.id}>`) {
-					const msg = random[Math.floor(Math.random() * 9)];
-					message.channel.send((msg == ' Hentai!') ? `<@${message.author.id}> ${msg}`
-						: `${msg}`);
-				}
-				// Respon ketika ada ucapan Pagi dan malam
-				else if (message.content.toLowerCase().match(/\bohayou\b|\bpagi\b/i)) {
-					if (moment().hour() >= 11 && moment().hour() < 16) {
-						message.channel.send(`<@${message.author.id}>-san, sudah siang loh...`);
-					}
-					else if (moment().hour() >= 16 && moment().hour() < 19) {
-						message.channel.send(`<@${message.author.id}>-san, sudah sore loh...`);
-					}
-					else if (moment().hour() >= 19 && moment().hour() < 24) {
-						message.channel.send(`<@${message.author.id}>-san, sudah malam loh...`);
-					}
-					else if (message.author.id === process.env.REI_ID) {
-							message.channel.send('Ohayou, Goshuujin-sama');
-					}
-					else {
-						message.channel.send(`Ohayou, <@${message.author.id}>-san`);
-					}
-				}
-				else if (message.content.toLowerCase().match(/\boyasumi\b|\bgood night\b/i)) {
-					if (moment().hour() >= 4 && moment().hour() < 11) {
-						message.channel.send(`<@${message.author.id}>-san, sudah siang loh...`);
-					}
-					else if (moment().hour() >= 11 && moment().hour() < 16) {
-						message.channel.send(`<@${message.author.id}>-san, sudah sore loh...`);
-					}
-					else if (moment().hour() >= 16 && moment().hour() < 19) {
-						message.channel.send(`<@${message.author.id}>-san, sudah malam loh...`);
-					}
-					else if (message.author.id === process.env.REI_ID) {
-						message.channel.send(`Hai, Hizamakura desu! Hehe... Yoshi yoshi~ <@${message.author.id}>-sama! Oyasumi`);
-						message.channel.send('https://cdn.discordapp.com/attachments/853972029983293440/963486674749366352/unknown.png');
-					}
-					else if (message.author.id === process.env.MOON_ID) {
-						message.channel.send(`Hehehe, there you are bedtime♪ Have a nice, peaceful sleep! <@${message.author.id}>-san! Daisuki yo...`);
-						message.channel.send('https://media.discordapp.net/attachments/955866146060517416/963305097864044596/unknown.png');
-					}
-					else {
-						message.channel.send(`Oyasumi, <@${message.author.id}>-san`);
-						message.channel.send('https://cdn.discordapp.com/attachments/955866146060517416/963302540416520212/unknown.png');
-					}
-				}
-				// Respon ucapan lainnya
-				else if (message.content.toLowerCase().match(/\btag kyamete\b|\bkawai\b/i)) {
-					message.channel.send(`Mooo.... hazukashii yo, <@${message.author.id}>-san`);
-				}
-				else if (message.content.toLowerCase().match(/\bteteh itu siapa?\b/i)) {
-					message.channel.send('Trainer kesayangan manhattan cafe');
-				}
-				else if (message.content.toLowerCase().match(/\b:zeana:\b/i)) {
-					message.channel.send('なんでやねん！');
-				}
-				else {
-					message.channel.send('Maaf, ATRI masih belum mengerti');
-				}
-			}
-		}
-		catch (err) {
-			console.error(err);
-			logger.error(err);
-		}
-	},
+        // Get responses from chat dataset
+        const content = message.content.toLowerCase();
+
+        if (content === `<@${client.user.id}>`) {
+          const conversation = conversations.find(
+              (conversation) => conversation.option === 'mention',
+          );
+          const responseLength = conversation.response.length;
+          const response =
+            conversation.response[Math.floor(Math.random() * responseLength)];
+
+          if (response.includes('Gohan')) {
+            const gohanResponse = await message.reply(response);
+            const conversation = conversations.find(
+                (conversation) => conversation.option === 'mention gohan',
+            );
+            return setTimeout(() => {
+              const response =
+                conversation.response[Math.floor(Math.random() * 2)];
+              gohanResponse.reply(response);
+            }, 3000);
+          }
+          return await message.reply(response);
+        } else {
+          const conversation = conversations.find((conversation) =>
+            conversation.utterance.find((utterance) =>
+              content.includes(utterance),
+            ),
+          );
+
+          if (conversation === undefined) {
+            return await message.reply('Maaf, ATRI masih belum mengerti');
+          } else if (conversation.hasOwnProperty('option')) {
+            if (conversation.option === 'atri birthday') {
+              const currentDate = new Date();
+              if (currentDate.getMonth() === 7 && currentDate.getDay() === 28) {
+                return await message.channel.send(
+                    conversation.response.replace('USERID', message.author.id),
+                );
+              } else {
+                return await message.channel.send(
+                    'Ini bukan tanggal ulang tahunku...',
+                );
+              }
+            } else if (
+              ['morning greeting', 'night greeting'].includes(
+                  conversation.option,
+              )
+            ) {
+              const currentUTCDate = new Date();
+              const currentDate = new Date(
+                  currentUTCDate.toLocaleString('en-US', {
+                    timeZone: 'Asia/Jakarta',
+                  }),
+              );
+              if (
+                conversation.option === 'morning greeting' &&
+                currentDate.getHours() <= 10
+              ) {
+                if (message.author.id === process.env.REI_ID) {
+                  return await message.reply(conversation.response[1]);
+                }
+                return await message.reply(
+                    conversation.response[0].repeat(
+                        'USERID', message.author.id,
+                    ),
+                );
+              } else if (
+                conversation.option === 'night greeting' &&
+                currentDate.getHours() >= 19
+              ) {
+                if (message.author.id === process.env.REI_ID) {
+                  await message.reply(
+                      conversation.response[1].message.replace(
+                          'REI',
+                          message.author.id,
+                      ),
+                  );
+                  return await message.channel.send(
+                      conversation.response[1].embed,
+                  );
+                } else if (message.author.id === process.env.MOON_ID) {
+                  await message.reply(
+                      conversation.response[2].message.replace(
+                          'MOON',
+                          message.author.id,
+                      ),
+                  );
+                  return await message.channel.send(
+                      conversation.response[2].embed,
+                  );
+                }
+                await message.reply(
+                    conversation.response[0].message.replace(
+                        'USERID',
+                        message.author.id,
+                    ),
+                );
+                return await message.channel.send(
+                    conversation.response[0].embed,
+                );
+              } else {
+                const responses = conversations.find((conversation) =>
+                  conversation.option === 'invalid greeting').response;
+
+                for (const response of responses) {
+                  if (currentDate.getHours() <= response.maxHour) {
+                    return await message.reply(
+                        response.response.replace('USERID', message.author.id),
+                    );
+                  }
+                }
+              }
+            }
+          } else {
+            return await message.reply(conversation.response);
+          }
+        }
+      }
+    } catch (err) {
+      console.error(err);
+      logger.error(err);
+    }
+  },
 };
